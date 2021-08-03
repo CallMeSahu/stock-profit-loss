0fb00ccbbbda78b6496a316e9f164f77f040531a
Siddhartha <siddharthasahu.work@gmail.com>
2021-08-04 00:30:41 +0530

    fix: blank input

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var checkBtn = document.querySelector("#btn-check");
 var output = document.querySelector("#output");
 
 checkBtn.addEventListener("click", () => {
+    if((costPriceInput.value == "")||(quantityInput.value == "")||(currentPriceInput.value == "")){
+        alert("Fill all blanks!");
+        return;
+    }
+
     var costPrice = parseInt(costPriceInput.value, 10);
     var quantity = parseInt(quantityInput.value, 10);
     var currentPrice = parseInt(currentPriceInput.value, 10);
@@ -13,13 +18,15 @@ checkBtn.addEventListener("click", () => {
     var sold = currentPrice * quantity;
     
     if(costPrice <= currentPrice){
+        var profit = (sold - buy);
         var percentage = (((currentPrice - costPrice )/ currentPrice)*100).toFixed(2); 
-        var result = `Your made a PROFIT 🙌 of ${percentage}%`;
+        var result = `You gained ${percentage}% 🙌. Your total profit is ₹${profit}`;
         output.innerText = result;
     }
     else{
+        var loss = (buy - sold);
         var percentage = (((costPrice - currentPrice)/ costPrice)*100).toFixed(2); 
-        var result = `Your made LOSS 😟 of ${percentage}%`;
+        var result = `You lost ${percentage}% 😔. Your total loss is ₹${loss}`;
         output.innerText = result;
     }
 
